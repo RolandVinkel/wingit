ceb09948271d6abf7c33ded83d618c3c79d46af0
Roland <roland.vinkel@voco.ee>
2022-11-09 01:41:19 -0800

    kasutajaloomine2

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,12 @@
+﻿$Fail = "C:\Users\Administrator\Desktop\kasutajad.csv"
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+foreach ($kasutaja in $kasutajad)
+{
+$Kasutajanimi = $kasutaja.Kasutajanimi
+$Taisnimi = $kasutaja.Taisnimi
+$Kontokirjeldus = $kasutaja.KontoKirjeldus
+$Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+New-LocalUser "$Kasutajanimi" -Password $Parool -Fullname "$Taisnimi" -Description "$Kontokirjeldus"
+}
+
+
