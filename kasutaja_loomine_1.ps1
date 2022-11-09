9615190b63ffe422868e49bcfef0389eff280650
Roland <roland.vinkel@voco.ee>
2022-11-09 00:49:12 -0800

    sus

---
+++ kasutaja_loomine_1.ps1
@@ -0,0 +1,11 @@
+﻿#defineerime vajaliku käsurea parameetrid
+param(
+$Kasutajanimi,
+$Taisnimi,
+$KontoKirjeldus
+)
+#loome kasutajale parooli
+$KasutajaParool = ConvertTo-SecureString "Qweasdzxc1!" -AsPlainText -Force
+#lisame kasutaja vastavate andmetega
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+#.\kasutaja_loomine_1.ps1 kasutaja2 "esimene kasutaja" "Konto lokaalseks kasutamiseks"
