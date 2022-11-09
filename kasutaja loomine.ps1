0bae4ea5c31d2d53d7211724dfcfd074b5849fcf
Roland <roland.vinkel@voco.ee>
2022-11-09 00:52:43 -0800

    sus

---
+++ kasutaja loomine.ps1
@@ -0,0 +1,3 @@
+﻿$KasutajaParool = convertto-securestring "Qweasdzxc1!" -asplaintext -force
+
+New-LocalUser "Kasutaja1" -Password $KasutajaParool -fullname "roland" -Description "local account - kasutaja1"
