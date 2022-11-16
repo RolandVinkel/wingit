27e2431b84857cc91aa28d26262b9605f94b3ffa
Roland <roland.vinkel@voco.ee>
2022-11-16 06:32:19 -0800

    Tunnitöö

---
+++ Ul1_lokaalse kasutaja loomine..ps1
@@ -0,0 +1,20 @@
+﻿Echo "Sisesta Eesnimi"
+$eesnimi = Read-Host
+Echo "Sisesta perenimi"
+$perenimi = Read-Host
+$kasutajanimi = $eesnimi.ToLower() + "." + $perenimi.ToLower()
+$taisnimi = $eesnimi + "." + $perenimi
+Echo "loodav kasutaja on $kasutajanimi"
+Echo "Lisa kommentaar"
+$kommentaar = Read-Host   
+Echo "Sisesta password"
+$password = Read-Host -AsSecureString
+$ErrorActionPreference = "Silentlycontinue"
+New-LocalUser -Name $kasutajanimi -Password $password -FullName $taisnimi -Description $kommentaar 
+if(!$?) {
+Echo "viga, tee õieti."
+}
+ErrorActionPreference = 'Stop'
+
+
+
---
+++ Ulesanne2-Lokaalsete kasutajate kustutamine süsteemist.ps1
@@ -0,0 +1,14 @@
+﻿Echo "Sisesta Eesnimi"
+$eesnimi = Read-Host
+Echo "Sisesta perenimi"
+$perenimi = Read-Host
+$kasutajanimi = $eesnimi.ToLower() + "." + $perenimi.ToLower()
+Echo "kustutav kasutaja on $kasutajanimi"
+$ErrorActionPreference = "Silentlycontinue"
+Remove-LocalUser -Name $kasutajanimi
+if(!$?) {
+Echo "Tekkinud probleem kasutaja kustutamisega"
+}else{
+Echo "kasutaja kustutatud"
+}
+ErrorActionPreference = 'Stop'
